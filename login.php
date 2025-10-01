f84ae77181d71d9fa00ba8383974d0c8e255d51e
Aysu Maharramova <125694685+aysumaharramovaa@users.noreply.github.com>
2025-10-01 23:13:17 +0400

    login and register php

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+include('db.php');
+session_start();
+
+$user = $_POST["user"];
+$pass = $_POST['pass'];
+
+$sql = "SELECT *FROM users WHERE username='$user'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+ while($row = $result->fetch_assoc()) {
+  $sifrelenmis = $row['password'];
+$name = $row['name'];
+$usert_type = $row['user_type'];
+
+  if(password_verify($pass,$sifrelenmis)){
+    $_SESSION['ad'] = $name;
+    $_SESSION['type'] = $usert_type;
+    header("location:home.php");
+    exit();
+  }   
+
+  }
+} else {
+  echo "Username or Password invalid!";
+}
+$conn->close();
+?>
---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+include('db.php');
+
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$user = $_POST["user"];
+$pass = $_POST['pass'];
+$sifrelenmis = password_hash($pass,PASSWORD_DEFAULT);
+$user_type = $_POST['user_type'];
+
+
+$sql = "INSERT INTO users (username, password, name,surname,user_type) VALUES ('$user', '$sifrelenmis', '$name','$surname','$user_type')";
+
+if ($conn->query($sql) === TRUE) {
+ header('location:index.html');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
